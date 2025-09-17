da5f5730bcc193e5f77f000f2e416eec0494f03d
biodiscus <biodiscus@gmail.com>
2025-09-18 01:02:09 +0200

    test: increase timeout limits for CI

--- vitest.config.js
+++ vitest.config.js
@@ -13,8 +13,16 @@ export default {
       deps: {
         inline: [
           'AssertionError: ts',
-        ]
-      }
+        ],
+      },
     },
+
+    // CI runners are slower, give more time and reduce threading
+    testTimeout: process.env.CI ? 20000 : 5000,
+    hookTimeout: process.env.CI ? 10000 : 5000,
+    threads: !process.env.CI,
+
+    // Less noisy logs on CI, keep logs locally
+    silent: !!process.env.CI,
   },
-}
+};
